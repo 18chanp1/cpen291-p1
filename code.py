bb918c205a3f0c2c6a2390058099514efb9d8ee5
van1011 <vanyquon@gmail.com>
2022-02-16 21:45:38 -0800

    fixed indent

--- code.py
+++ code.py
@@ -19,52 +19,57 @@ r_leg = servo.Servo(pwm_r_leg)
 #REMEMBER SERVOS ARE BY DEFAULT AT 90
 
 #move legs in straight line, lift feet
-def split(loops): 
-	for i in range(loops):
-		for angle in range(90, 0, -5):  #don't remember which way it rotates
-	        l_leg.angle = angle
-	        time.sleep(0.05)
-	    for angle in range(90, 180, 5):  
-	        r_leg.angle = angle
-	        time.sleep(0.05)
+def split(loops):
+        for i in range(loops):
+                for angle in range(90, 0, -5):  #don't remember which way it rotates
+                        l_leg.angle = angle
+                        time.sleep(0.05)
+                for angle in range(90, 180, 5):
+                        r_leg.angle = angle
+                        time.sleep(0.05)
 
-	    for angle in range(90, 45, -5):  #don't remember which way it rotates
-	        l_foot.angle = angle
-	        time.sleep(0.05)
-	    for angle in range(90, 135, 5):  
-	        r_foot.angle = angle
-	        time.sleep(0.05)
+                for angle in range(90, 45, -5):  #don't remember which way it rotates
+                        l_foot.angle = angle
+                        time.sleep(0.05)
+                for angle in range(90, 135, 5):
+                        r_foot.angle = angle
+                        time.sleep(0.05)
 
-	    time.sleep(0.5)
+                time.sleep(0.5)
 
-	    for angle in range(45, 90, 5):  #don't remember which way it rotates
-	        l_foot.angle = angle
-	        time.sleep(0.05)
-	    for angle in range(135, 90, -5):  
-	        r_foot.angle = angle
-	        time.sleep(0.05)
+                for angle in range(45, 90, 5):  #don't remember which way it rotates
+                        l_foot.angle = angle
+                        time.sleep(0.05)
+                for angle in range(135, 90, -5):
+                        r_foot.angle = angle
+                        time.sleep(0.05)
 
-	    for angle in range(0, 90, 5):  #don't remember which way it rotates
-	        l_leg.angle = angle
-	        time.sleep(0.05)
-	    for angle in range(180, 90, -5):  
-	        r_leg.angle = angle
-	        time.sleep(0.05)
+                for angle in range(0, 90, 5):  #don't remember which way it rotates
+                        l_leg.angle = angle
+                        time.sleep(0.05)
+                for angle in range(180, 90, -5):
+                        r_leg.angle = angle
+                        time.sleep(0.05)
 
-	    time.sleep(0.5)
+                time.sleep(0.5)
 
 def stuttered_twist(loops):
-	for i in range(loops):
-		for angle in range(90, 0, -5):  #shift legs inward
-	        l_leg.angle = angle
-	        r_leg.angle = 180 - angle
-	        time.sleep(0.05)
+        for i in range(loops):
+                for angle in range(90, 0, -5):  #shift legs inward
+                        l_leg.angle = angle
+                        r_leg.angle = 180 - angle
+                        time.sleep(0.05)
 
-	    for angle in range(0, 90, 5):  #shift legs outward
-	        l_leg.angle = angle
-	        r_leg.angle = 180 - angle
-	        time.sleep(0.05)
+                for angle in range(0, 90, 5):  #shift legs outward
+                        l_leg.angle = angle
+                        r_leg.angle = 180 - angle
+                        time.sleep(0.05)
 
-	    time.sleep(0.5)
+                time.sleep(0.5)
 
+while(true):
+       split(5);
+       time.sleep(2);
+       sleep(5);
+       time.sleep(2);
 
