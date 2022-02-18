640653624e520e14d726ae933ab60618c9dc792d
Vanessa Quon <77385101+van1011@users.noreply.github.com>
2022-02-18 09:03:01 -0800

    Update code.py
    extra while loop deleted

--- code.py
+++ code.py
@@ -67,12 +67,6 @@ def stuttered_twist(loops):
 
         time.sleep(0.5)
 
-while(true):
-       split(5);
-       time.sleep(2);
-       sleep(5);
-       time.sleep(2);
-
 while(True):
    split(3)
    time.sleep(2)
